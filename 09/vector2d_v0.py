7f2450a5128703e56e793bb31f9f953ce0506117
lagougou <jiangruitc@163.com>
2018-12-14 18:27:51 +0800

    add chapter 09 demo

---
+++ 09/vector2d_v0.py
@@ -0,0 +1,59 @@
+from array import array
+import math
+
+class Vector2d:
+    typecode = 'd'
+
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+
+    def __iter__(self):
+        return (i for i  in (self.x, self.y))
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return '{}({!r},{!r})'.format(class_name, *self)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) + bytes(array(self.typecode, self)))
+
+    def __eq__(self,other):
+        return type(self) == tuple(other)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __format__(self, fmt_spec=''):
+        if fmt_spec.endswith("p"):
+            fmt_spec = fmt_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:
+            coords = self
+            outer_fmt = "Vecotor({},{})"
+        x, y = coords
+        return outer_fmt.format(format(x, fmt_spec),format(y, fmt_spec))
+
+    def angle(self):
+        return math.atan2(self.x, self.y)
+
+    
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:].cast(typecode))
+        return cls(*memv)
+
+vector = Vector2d(3,4)
+
+print(format(vector,".3fp"))
+for x in vector:
+    print(x)
+
